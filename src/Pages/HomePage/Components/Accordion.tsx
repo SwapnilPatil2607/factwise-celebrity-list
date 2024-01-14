4c5db51a5dcd4f6d15b188e7370f17bda180b896
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2024-01-14 14:44:18 +0530

    feat: open accordion structure ready

--- src/Pages/HomePage/Components/Accordion.tsx
+++ src/Pages/HomePage/Components/Accordion.tsx
@@ -3,11 +3,10 @@ import styled from "styled-components";
 
 const Container = styled.div`
   width: 400px;
-  display: flex;
+
   padding: 10px 15px;
   border-radius: 10px;
   border: 1px solid black;
-  justify-content: space-between;
 `;
 
 const UserTitle = styled.div`
@@ -28,14 +27,76 @@ const ProfilePictureContainer = styled.div`
   border: 1px solid black;
 `;
 
+const ClosedViewContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const UserDetails = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  text-align: left;
+  padding: 15px 0px;
+  div {
+    width: 100%;
+  }
+`;
+
+const Description = styled.div`
+  width: 100%;
+`;
+
+const ActionButtons = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  gap: 10px;
+`;
+
 const Accordion = () => {
   return (
     <Container>
-      <UserTitle>
-        <ProfilePictureContainer>image</ProfilePictureContainer>
-        <div>Name</div>
-      </UserTitle>
-      <button>+</button>
+      <ClosedViewContainer>
+        <UserTitle>
+          <ProfilePictureContainer>
+            <img src="" />
+          </ProfilePictureContainer>
+          <div>Name</div>
+        </UserTitle>
+        <button>+</button>
+      </ClosedViewContainer>
+
+      <div>
+        <UserDetails>
+          <div>
+            <div>Age</div>
+            <div>value</div>
+          </div>
+          <div>
+            <div>Gender</div>
+            <div>value</div>
+          </div>
+          <div>
+            <div>Country</div>
+            <div>value</div>
+          </div>
+        </UserDetails>
+
+        <Description>
+          <h5>Label</h5>
+          <div>Value</div>
+        </Description>
+      </div>
+
+      <ActionButtons>
+        <button>Delete</button>
+        <button>Edit</button>
+      </ActionButtons>
     </Container>
   );
 };
